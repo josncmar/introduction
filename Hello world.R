c77402ca0fbd8d9baae7ce7b180f58231de4a2bf
Sánchez <jose.carlos.sanchez@accenture.com>
2019-07-31 17:35:14 +0200

    Prueba

--- Hello world.R
+++ Hello world.R
@@ -9,13 +9,3 @@ murders %>%
     ggplot(aes(population, total, label = abb, color = region)) + 
     geom_label()
 
-#Funciona?
-
-install.packages("dslabs")
-library(tidyverse)
-library(dslabs)
-data(murders)
-
-murders %>%
-  ggplot(aes(population, total, label = abb, color = region)) + 
-  geom_label()
