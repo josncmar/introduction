5089aa137039a9c8802092bf73bc3acb1a60bd0c
Sánchez <jose.carlos.sanchez@accenture.com>
2019-08-01 15:51:45 +0200

    update_0108

--- Hello world.R
+++ Hello world.R
@@ -24,7 +24,7 @@ data()
 co2
 save.image("~/introduction/introduction.RData")
 
-# Exercises
+# exercises
 
 n=100
 positive = n*(n+1)/2
@@ -40,20 +40,70 @@ x
 sum(x)
 
 
-help(seq)
+help("log")
+
+log(100,base=10)
+log(exp(2))
+n <- 1000
+class(n)
+
+# dataframes
+
+dat <- data(murders)
+str(murders)
+head(murders)
+pop <- as.integer(murders$population)
+names(murders)
+length(pop)
+class(pop)
+z <- 3 == 2  
+z
+f <- 2
+class(fL)
+as.integer(pop)
+pop
+class(pop)
+class(murders$region)
+levels(murders$region)
+
+region <- murders$region
+levels(region)
+value <- murders$total
+region <- reorder(region, value, FUN = sum)
+levels(region)
+
+mat <- matrix(1:12, 4, 3)
+mat
+
+mat[2, 3]
+mat[2, ]
+mat[, 2:3]
+dataframe <- as.data.frame(mat)
+class(mat)
+class(dataframe)
+dataframe
+mat
+
+# exercises
+
+library(dslabs)
+data(murders)
+
+str(murders)
+names(murders)
+
+a <- murders$abb
+a
+class(a)
+b <-murders[,2]
+b
+
+c <- a==b
+c
+
+class(murders$region)
+
+length(levels(region))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+table(murders$region)
